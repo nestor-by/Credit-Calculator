34f68306c7a5ffc57da698af62bb2f40404385f2
DenHaz <denis47-90@mail.ru>
2014-01-25 17:29:56 +0600

    JavaFX: Добавление логики на новый GUI
    JavaFX: Добавление логики на новый GUI

--- fxGUI/src/main/java/sample/Controller.java
+++ fxGUI/src/main/java/sample/Controller.java
@@ -3,19 +3,22 @@ package sample;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import su.ugatu.moodle.is.credit_calc.*;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 
 public class Controller {
-
+    private CreditApplication application;
+    private CreditOffer offer;
     private ObservableList<CreditData> creditData = FXCollections.observableArrayList();
     private ObservableList<String> model = FXCollections.observableArrayList();
     @FXML
@@ -58,6 +61,8 @@ public class Controller {
     private TextField monthlyCommissionPercent;
 
 
+
+
     @FXML
     private void initialize() {
 
@@ -86,39 +91,45 @@ public class Controller {
     }
 
     private void initData() {
-        creditData.add(new CreditData(1, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(2, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(3, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(4, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(5, "Alex", "qwerty", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(5, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(6, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(7, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(8, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(9, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(10, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(11, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
-        creditData.add(new CreditData(12, "Alex", "120.56", "alex@mail.com", "Hello", "123", "9900"));
+
+        if(combo_type.getSelectionModel().isSelected(1)){
+            application.setPaymentType(CreditPaymentType.DIFFERENTIAL);
+            CreditProposal proposal = offer.calculateProposal(application);
+            printProposal(proposal);
+        }
+        else{
+            application.setPaymentType(CreditPaymentType.ANNUITY);
+            CreditProposal proposal = offer.calculateProposal(application);
+            printProposal(proposal);
+        }
     }
+
     @FXML
     public void onClickMethod(){
+        creditData.clear();
         if(control(amount) && control(durationInMonths) && control(interestRate)){
-            if(!control(onceCommissionAmount))
-                onceCommissionAmount.setText("0");
-            if(!control(onceCommissionPercent))
-                onceCommissionPercent.setText("0");
-            if(!control(monthlyCommissionAmount))
-                monthlyCommissionAmount.setText("0");
-            if(!control(monthlyCommissionPercent))
-                monthlyCommissionPercent.setText("0");
+            application = new CreditApplicationImpl(new BigDecimal(amount.getText()));
+            application.setDurationInMonths(Integer.valueOf(durationInMonths.getText()));
+            offer = new CreditOfferImpl();
+            offer.setRate(new BigDecimal(interestRate.getText()).divide(new BigDecimal(100)));
 
-            if(combo_type.getSelectionModel().isSelected(1)){
-                System.out.print("deffer");
-            }
-            else{
-                System.out.print("annut");
-            }
-            creditData.clear();
+            if(control(onceCommissionAmount))
+                offer.setOnceCommissionAmount(new BigDecimal(onceCommissionAmount.getText()));
+            else
+                onceCommissionAmount.setText("0");
+            if(control(onceCommissionPercent))
+                offer.setOnceCommissionPercent(new BigDecimal(onceCommissionPercent.getText()).divide(new BigDecimal(100)));
+            else
+                onceCommissionPercent.setText("0");
+            if(control(monthlyCommissionPercent))
+                offer.setMonthlyCommissionPercent(new BigDecimal(monthlyCommissionPercent.getText()).divide(new BigDecimal(100)));
+            else
+                monthlyCommissionPercent.setText("0");
+            if(control(monthlyCommissionAmount))
+                offer.setMonthlyCommissionAmount(new BigDecimal(monthlyCommissionAmount.getText()));
+            else
+                monthlyCommissionAmount.setText("0");
+
             initData();
         }
 
@@ -127,6 +138,11 @@ public class Controller {
     public void onExit(){
         System.exit(0);
     }
+
+//    @FXML
+//    public void onClickHelp(){
+//        System.exit(0);
+//    }
 
     @FXML
     public void onHover(){
@@ -159,6 +175,27 @@ public class Controller {
             return false;
         }
         return true;
+    }
+
+    private void printProposal(CreditProposal proposal){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        List<CreditPayment> payments = proposal.getPayments();
+        text_res.setText("Всего: "+decimalFormat.format(proposal.getTotalPayment())+" денежных единиц\n"
+                        +"Эффективная процентная ставка: "+(decimalFormat.format(proposal.getEffectiveRate().doubleValue() * 100)) + "%\n"
+                        +"Комиссия: " + decimalFormat.format(proposal.getInitialCreditCommission()));
+        int i = 1;
+        for (CreditPayment payment: payments) {
+            creditData.add(new CreditData(
+                    (i++),
+                    "" + dateFormat.format(payment.getDate()),
+                    "" + decimalFormat.format(payment.getAmount()),
+                    "" + decimalFormat.format(payment.getDebt()),
+                    "" + decimalFormat.format(payment.getInterest()),
+                    "" + decimalFormat.format(payment.getCommission()),
+                    "" + decimalFormat.format(payment.getTotalLeft())));
+        }
+
     }
 
 }
